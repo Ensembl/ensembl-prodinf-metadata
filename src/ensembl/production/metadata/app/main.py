982289475eb214876629c8bc9db66077726791ad
Marc Chakiachvili <mchakiachvili@ebi.ac.uk>
2021-05-20 21:20:54 +0100

    Changed

--- src/ensembl/production/metadata/app/main.py
+++ src/ensembl/production/metadata/app/main.py
@@ -13,7 +13,7 @@ static_path = os.path.join(app_path, 'static')
 template_path = os.path.join(app_path, 'templates')
 
 app = Flask(__name__,
-            static_url_path='',
+            static_url_path='/static/metadata',
             static_folder=static_path,
             template_folder=template_path,
             instance_relative_config=True)
